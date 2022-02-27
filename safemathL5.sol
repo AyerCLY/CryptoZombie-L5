bb501189ed79cdb4a56f87d909d6335da43b1124
Ayer CLY <95411578+AyerCLY@users.noreply.github.com>
2022-02-27 17:30:28 +0800

    Update safemathL5.sol

--- safemathL5.sol
+++ safemathL5.sol
@@ -1,4 +1,12 @@
 pragma solidity >=0.5.0 <0.6.0;
+//What's an overflow?
+//Let's say we have a uint8, which can only have 8 bits. That means the largest number we can store is binary 11111111 (or in decimal, 2^8 - 1 = 255).
+//Take a look at the following code. What is number equal to at the end?
+//uint8 number = 255;
+//number++;
+//In this case, we've caused it to overflow — so number is counterintuitively now equal to 0 even though we increased it. (If you add 1 to binary 11111111, it resets back to 00000000, like a clock going from 23:59 to 00:00).
+//An underflow is similar, where if you subtract 1 from a uint8 that equals 0, it will now equal 255 (because uints are unsigned, and cannot be negative).
+//While we're not using uint8 here, and it seems unlikely that a uint256 will overflow when incrementing by 1 each time (2^256 is a really big number), it's still good to put protections in our contract so that our DApp never has unexpected behavior in the future.
 
 /**
  * @title SafeMath
